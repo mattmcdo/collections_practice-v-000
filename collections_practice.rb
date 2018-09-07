6c0d17251c9eb0965faf742f15a78de98c60fd90
Matthew McDonald <matthew.mcd1031@gmail.com>
2018-09-07 21:27:41 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+def sort_array_asc(array)
+array.sort
+end
 
+def sort_array_desc(array)
+array.sort {|a,b| b <=> a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+end
+
+def find_a(array)
+  array.select do |x|
+    x.start_with?("a")
+end
+end
+
+def sum_array(array)
+  array.inject do |x,y|
+    x + y
+end
+end
+
+def add_s(my_array)
+my_array.each_with_index.collect do |x, i|
+  if i == 1
+ x
+  else
+  x + "s"
+end
+end
+end
